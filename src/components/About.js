938ae4112459f8ae70637d5fac3187678878a473
Roisin Pearson <roisin.pearson@xero.com>
2019-09-09 17:52:53 +1200

    Added navigation and about page

---
+++ src/components/About.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+
+class About extends Component {
+    render() {
+        return (
+
+            <div class="about">
+                <h2> ABOUT ME </h2>
+                <h3> Hi I'm Roisin </h3>
+                <p> I am a Victoria University of Wellington design student who has recently returned from studying
+                    abroad at Carnegie Mellon University in the U.S.
+                    I was born and raised in Napier, and now live in Wellington, NZ! </p>
+                <p>
+                    Technology focused design is what I am most passionate about, espescially web design and
+                    development.
+                    I am interested in where code meets design and feel that knowledge of these two practises is
+                    becoming
+                    more useful in our technologically advancing world.
+                </p>
+
+                <p> Aside from designing things for the web, I also enjoy making physical interactive technologies.
+                    Creating devices with Arduino or the Particle Photon allows me to continuously come up with new
+                    ideas
+                    and brainstorm future possibilites for our world. </p>
+
+                <p> I'm currently looking for a full time job for 2019! </p>
+            </div>
+        );
+    }
+}
+
+export default About;
