337c683dfa60afce07d4ee3e1cea3a9baeeb8f63
Roisin Pearson <roisin.pearson@xero.com>
2020-01-27 09:20:39 +1300

    Change background colour for different pages

--- src/App.js
+++ src/App.js
@@ -1,34 +1,59 @@
-import React from 'react';
-import banner from './components/images/banner.png';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import React, { useState } from "react";
+import banner from "./components/images/banner.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 import About from "./components/About";
 import { Home } from "./components/Home";
 import Contact from "./components/Contact";
-import Navbar from 'react-bootstrap/Navbar'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-
+import Navbar from "react-bootstrap/Navbar";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
+  const [color, changeColor] = useState("#282c34");
   return (
-<Router>
+    <div style={{ background: color }} id="main">
+      <Router>
+        <Navbar bg="dark" variant="dark" className="navbar">
+          <ul className="navbar-nav mr-auto">
+            <li>
+              <Link
+                to={"/"}
+                onClick={() => changeColor("#fff")}
+                className="nav-link"
+              >
+                {" "}
+                Home{" "}
+              </Link>
+            </li>
+            <li>
+              <Link
+                to={"/about"}
+                onClick={() => changeColor("#02132b")}
+                className="nav-link"
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                to={"/contact"}
+                onClick={() => changeColor("#B761AA")}
+                className="nav-link"
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </Navbar>
 
-          <Navbar bg="dark" variant="dark" className="navbar">
-              <ul className="navbar-nav mr-auto">
-                  <li><Link to={'/'} className="nav-link"> Home </Link></li>
-                  <li><Link to={'/about'} className="nav-link">About</Link></li>
-                  <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
-              </ul>
-          </Navbar>
-
-          <Switch>
-              <Route exact path='/' component={Home} />
-              <Route path='/about' component={About} />
-              <Route path='/contact' component={Contact} />
-          </Switch>
-</Router>
-
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
--- src/components/About.js
+++ src/components/About.js
@@ -1,34 +1,42 @@
-import React, {Component} from 'react';
-import { Container, Row} from 'react-bootstrap';
+import React, { Component } from "react";
+import { Container, Row } from "react-bootstrap";
 
 class About extends Component {
-    render() {
-        return (
+  render() {
+    return (
+      <Container className="about">
+        <Row>
+          <div className="about col-6 mx-auto">
+            <h2 className="about-heading text-center"> ABOUT ME </h2>
+          </div>
+        </Row>
+        <Row>
+          <div className="about col-8 mx-auto">
+            <h3 className="about-heading"> Hi I'm Roisin </h3>
+            <p>
+              {" "}
+              I am a software developer with a passion for beautiful design.{" "}
+            </p>
+            <p>
+              My studies in design and information systems means I have the
+              skills to both develop applications and design them. The worlds of
+              design and development have previously been relatively separate
+              from each other so my aim is to bridge the gap between the two
+              professions and give equal importance to both.
+            </p>
 
-            <Container>
-                <Row>
-                    <div className="about col-6 mx-auto">
-                <h2 className="text-center"> ABOUT ME </h2>
-                    </div>
-                </Row>
-                <Row>
-                    <div className="about col-8 mx-auto">
-                <h3> Hi I'm Roisin </h3>
-                <p> I am a software developer with a passion for beautiful design. </p>
-                <p>
-                    My studies in design and information systems means I have the skills to both develop applications and design them.
-                    The worlds of design and development have previously been relatively separate from each other so my aim is to bridge the gap between the two professions
-                    and give equal importance to both.
-                </p>
+            <p>
+              {" "}
+              Aside from tech stuff I love ransacking thrift stores, dancing to
+              Rhianna and eating peanut butter toast.{" "}
+            </p>
 
-                <p> Aside from tech stuff I love ransacking thrift stores, dancing to Rhianna and eating peanut butter toast. </p>
-
-                <p> I am currently working as a graduate developer at Xero. </p>
-                    </div>
-                </Row>
-            </Container>
-        );
-    }
+            <p> I am currently working as a graduate developer at Xero. </p>
+          </div>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default About;
